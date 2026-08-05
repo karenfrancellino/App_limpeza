3653a97ee06af436b0015bfb883e3e852260a5b2
karenfrancellino <karenfrancellino@users.noreply.github.com>
2026-08-05 13:21:09 +0200

    Create calendar grid and appointment modal structure

--- app.js
+++ app.js
@@ -1,25 +1,42 @@
-const botao = document.getElementById("btn-client");
-const calendrier = document.getElementById("calendrier");
+const jours = document.querySelectorAll(".jour"); 
+const modal = document.getElementById("modal"); 
+const save = document.getElementById("save"); 
 
-botao.addEventListener("click", () =>{
-    const nome = prompt("Nom du client :");
-    const hora = prompt("Heure du service :");
-    const hora = document.getElementById("heure").value;
+let jourActif = null; 
 
-    if (!nome || !hora) {
-        return;
-    }
-
-    const evento = document.createElement("div");
-    evento.className = "evento";
-
-    evento.innerHTML = `
-        <strong>${nome}</strong>
-        <br>
-        ${hora}
-    `;
-
-    calendrier.appendChild(evento);
-    console.log(hora);
+// Abrir modal ao clicar no dia 
+jours.forEach(jour => { 
+    jour.addEventListener("click", () => { 
+        jourActif = jour;
+        modal.classList.remove("hidden"); 
+    }); 
+}); 
+// Salvar evento 
+save.addEventListener("click", () => { 
     
-});
+    const client = document.getElementById("client").value; 
+    const heure = document.getElementById("heure").value; 
+    const duree = document.getElementById("duree").value; 
+    const obs = document.getElementById("obs").value; 
+    const selectEmploye = document.getElementById("employe"); 
+    const employe = selectEmploye.options[selectEmploye.selectedIndex].text; 
+    const cor = selectEmploye.value; 
+    
+    if (!heure || !duree) return; 
+    
+    const evento = document.createElement("div"); 
+    evento.className = "evento"; 
+    
+    evento.style.borderLeftColor = cor; 
+    evento.innerHTML = ` 
+        <strong>${client}</strong> 
+        <br>👩 ${employe} 
+        <br>🕒 ${heure} 
+        <br>⏱ ${duree}h 
+        <br>📝 ${obs} 
+        `; 
+        
+        jourActif.appendChild(evento); 
+
+        modal.classList.add("hidden"); 
+    });
