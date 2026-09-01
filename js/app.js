79524ef0ddce28e6e70b5fa63f35d04ba9ff28e7
karenfrancellino <karenfrancellino@users.noreply.github.com>
2026-09-01 11:40:37 +0200

    Set up JavaScript module entry point

---
+++ js/app.js
@@ -0,0 +1,7 @@
+import { initAppointments } from "./appointments.js";
+
+function updateHoursCounter() {
+    // The hours counter will be implemented in another module.
+}
+
+initAppointments(updateHoursCounter);
